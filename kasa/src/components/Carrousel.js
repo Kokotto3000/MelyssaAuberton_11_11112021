bb91fa802870acaca5c208a014e52ae13f2c2030
Kokotto3000 <melyssa.auberton@gmail.com>
2022-03-27 23:09:35 +0200

    ajout d'un condition sur les boutons du carrousel pour qu'ils ne s'affichent pas s'il n'y a qu'une photo

--- kasa/src/components/Carrousel.js
+++ kasa/src/components/Carrousel.js
@@ -33,16 +33,20 @@ class Carrousel extends Component{
         return(
             <div className="carrousel">
                 <img className="carrousel_image" src={this.state.pictures[this.state.current]} alt={this.state.alt} />
-                <button className="carrousel_button-previous" onClick={this.handlePrevious.bind(this)}>
-                    <svg viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z" fill="white"/>
-                    </svg>
-                </button>
-                <button className="carrousel_button-next" onClick={this.handleNext.bind(this)}>
-                    <svg viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458Z" fill="white"/>
-                    </svg>
-                </button>
+                { this.state.pictures.length > 1 && 
+                    <button className="carrousel_button-previous" onClick={this.handlePrevious.bind(this)}>
+                        <svg viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z" fill="white"/>
+                        </svg>
+                    </button>
+                }
+                { this.state.pictures.length > 1 &&
+                    <button className="carrousel_button-next" onClick={this.handleNext.bind(this)}>
+                        <svg viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458Z" fill="white"/>
+                        </svg>
+                    </button> 
+                }
                 <p className="carrousel_index">{`${this.state.current + 1}/${this.state.pictures.length}`}</p>
             </div>
         );
